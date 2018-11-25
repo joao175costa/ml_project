ccaf7cb8b9253dd920685409fad695205eb1ca55
João Costa <joao175costa@gmail.com>
2018-11-25 23:13:48 +0000

    reader commit
    perform analysis on main.py, dataset reading on readers.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+from readers import read_dataset
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+
+X, Y = read_dataset()
+
+# quick test with SVM
+classifier = SVC()
+classifier.fit(X, Y)
+print(classification_report(Y, classifier.predict(X)))
---
+++ readers.py
@@ -0,0 +1,24 @@
+import numpy as np
+import os
+
+dataset_file = os.curdir + '/dataset/risk_factors_cervical_cancer.csv'
+
+
+def read_dataset(handle_sparse=True):
+    """
+    reads the risk factors cervical cancer dataset utilizing numpy.
+    handle_sparse=True substitutes the absent features for their dataset average
+    Biopsy results are considered as target variable
+    :returns: data (n_samples*n_feats), labels (n_samples)
+    """
+
+    raw_data = np.genfromtxt(dataset_file, delimiter=',', skip_header=1, missing_values='?')
+
+    data = raw_data[:, :-1]
+    if handle_sparse:
+        means = np.nanmean(data, axis=0)  # means of columns, ignoring nan
+        ix = np.where(np.isnan(data))  # indices of NaNs
+        data[ix] = np.take(means, ix[1])  # replace NaN with mean
+
+    labels = raw_data[:, -1]  # labels generated from the biopsy column
+    return data, labels
